9e2a456c161fe1c89b01a89501e4e4d6d8b563c7
cabanyes <47134139+cabanyes@users.noreply.github.com>
2020-11-06 11:40:03 +0100

    Update testWildfly.java

--- src/testWildfly/testWildfly.java
+++ src/testWildfly/testWildfly.java
@@ -4,7 +4,7 @@ public class testWildfly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world22 desde eclipse");
+		System.out.println("hello world22 desde eclipse y de nuevo a github");
 	}
 
 }
